26daa63381463fd2d494d2552916408aa70c2cc7
MinLee <minlee0702@gmail.com>
2020-11-27 23:13:14 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/services'
+                to='/projects'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
