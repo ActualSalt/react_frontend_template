2fc52eeedf611926c192ac00020933c2f184b128
MinLee <minlee0702@gmail.com>
2021-04-15 13:44:16 -0500

    custom card

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../../App.css';
+import Card from '../Card';
 
-export default function Projects(){
-    return <>
-    <h1 className='projects'>Projects</h1>
-    <p>Hello</p>
-    </>
-    ;
+
+function Projects(){
+    return(
+        <div>
+            <h3 className='Projects'>Projects</h3>
+        <div className='cards__container'>
+        <Card 
+        title='Title Here'
+        imageURL='https://cdn.vox-cdn.com/thumbor/RZOfEehCS-W-vC6IyMnv_Qfy_nI=/0x0:112x112/1200x0/filters:focal(0x0:112x112)/cdn.vox-cdn.com/uploads/chorus_asset/file/10775823/forsen_cd_first.png'
+        body='Body text here'
+        />
+        <Card 
+        title='Title Here'
+        imageURL='https://cdn.vox-cdn.com/thumbor/RZOfEehCS-W-vC6IyMnv_Qfy_nI=/0x0:112x112/1200x0/filters:focal(0x0:112x112)/cdn.vox-cdn.com/uploads/chorus_asset/file/10775823/forsen_cd_first.png'
+        body='Body text here'
+        />
+        </div>
+        </div>
+    );
 }
+
+export default Projects;
